180d54b7d432d6289ee3c85a19ebea59a0b4632a
Rushi Shah <2016rshah@gmail.com>
2014-12-06 18:49:58 -0500

    Publications

--- meteor-todos/server/server.js
+++ meteor-todos/server/server.js
@@ -5,8 +5,11 @@ if (Meteor.isServer) {
 		}
 	});
 	Meteor.publish('ToDos', function (userId) {
-		return ToDos.find({owner:userId})
+		return ToDos.find({owner:userId, completed:false})
 		console.log(userId)
 		//return ToDos.find({owner:userId})
 	});
+	Meteor.publish('Completed', function(userId){
+		return ToDos.find({owner:userId, completed:true})
+	})
 }
