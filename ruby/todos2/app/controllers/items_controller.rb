d29f2b3b848d808b71727b1d20f1362c5bdfa608
Rushi Shah <2016rshah@gmail.com>
2014-12-30 22:21:04 +0000

    Basic completed/uncompleted items

--- ruby/todos2/app/controllers/items_controller.rb
+++ ruby/todos2/app/controllers/items_controller.rb
@@ -1,10 +1,12 @@
 class ItemsController < ApplicationController
     
     def index
+        @item = Item.new
         @items = Item.all.order("created_at DESC")
     end
     
     def new
+        @item = Item.new
     end
     
     def create
@@ -20,7 +22,7 @@ class ItemsController < ApplicationController
     private
     
     def item_params
-        params.require(:item).permit(:content)
+        params.require(:item).permit(:content, :completed)
     end
     
 end
